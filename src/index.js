64b1d0d78c67878778014b996c86592156b84394
Bushra <khanbushras032@gmail.com>
2022-07-25 12:01:29 +0530

    modified

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const route = require('./route/route.js');
+const multer = require("multer");
+
+const { default: mongoose } = require('mongoose'); 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.multer().any();
+
+mongoose.connect("mongodb+srv://bushra:euVDEv190AGHYJDI@cluster0.nwfddcm.mongodb.net/group14Database?retryWrites=true&w=majority",{
+
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT|| 3000))
+});
---
+++ src/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema({
+        fname: {type: String, required:true, trim:true},
+        lname: {type: String, required:true, trim:true},
+        email: {type: String, required:true, unique: true, trim:true},
+        profileImage: {type: String, required:true, trim:true}, // s3 link
+        phone: {type: String, required:true, unique:true, trim:true}, 
+        password: {type: String, required: true, minLen: 8, maxLen: 15, trim:true}, // encrypted password
+        address: {
+          shipping: {
+            street: {type: String, required:true, trim:true},
+            city: {type: String, required:true, trim:true},
+            pincode: {type: number, required:true, trim:true}
+          },
+          billing: {
+            street: {type: String, required:true, trim:true},
+            city: {type: String, required:true, trim:true},
+            pincode: {type: number, required:true, trim:true}
+          }
+        },
+      }, { timestamps: true })
+      userSchema.pre('save', function (next) {
+        bcrypt.hash(this.password, 10).then((encryptedPassword) => {
+            this.password = encryptedPassword;
+            next();
+        }).catch((error) => {
+            throw error;
+        });
+    });
+module.exports = mongoose.model('User', userSchema)
