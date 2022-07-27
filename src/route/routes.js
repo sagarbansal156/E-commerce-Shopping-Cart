3260ef4b96803600045298a836e600781be27297
Girija <girija7682@gmail.com>
2022-07-27 16:52:28 +0530

    30%

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/userController")
+const productController = require("../controllers/productController")
+
 const {authentication,authorise} = require("../middleWare/auth")
 
 
@@ -9,6 +11,9 @@ router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/user/:userId/profile",authentication,userController.getUser);
 router.put("/user/:userId/profile",userController.updateUser);
+
+//----------product Api----------------//
+router.post("/products", productController.addProduct);
 
 
 
